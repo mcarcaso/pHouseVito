2a7958f474fa14013149274ef375e18eec1e4a18
Mike Carcasole <mikecarcasole@gmail.com>
2026-08-09 11:37:18 -0400

    refactor: remove orchestrator runtime barrel

--- src/services/orchestrator/PiOrchestratorService.ts
+++ src/services/orchestrator/PiOrchestratorService.ts
@@ -6,12 +6,10 @@
 import { parseInboundEventMetadata } from "../../contracts/inbound-event.js";
 import { CronScheduler } from "../../cron/scheduler.js";
 import type { Context } from "../../context/Context.js";
-import {
-  withPersistence,
-  withRelay,
-  withTracing,
-  withTyping,
-} from "./runtime/index.js";
+import { withPersistence } from "./runtime/PersistencePiRuntime.js";
+import { withRelay } from "./runtime/RelayPiRuntime.js";
+import { withTracing } from "./runtime/TracingPiRuntime.js";
+import { withTyping } from "./runtime/TypingPiRuntime.js";
 import { NoReplyOutputHandler } from "../../output/NoReplyOutputHandler.js";
 import { DirectChannelService } from "../channels/direct/DirectChannelService.js";
 import type { ChannelService } from "../channels/ChannelService.js";
